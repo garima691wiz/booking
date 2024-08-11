bc9c212f6d09cb65cbe2f9fb0ea582299c877408
Garima Majhi <90824307+garima691wiz@users.noreply.github.com>
2024-08-11 21:07:12 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,56 +1,26 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import bookingRouter from './routes/bookingRoutes.js';
-
-// Load environment variables
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import bookingRouter from "./routes/bookingRoutes.js";
 dotenv.config();
 
+
+//paste mongodbURI = "mongodb://127.0.0.1:27017/bookings" 
+
+const mongodbURI = process.env.MONGODBURI
 const app = express();
-const PORT = process.env.PORT || 8080;
-const mongodbURI = process.env.MONGODBURI;
+const PORT = 8080;
 
-// Check if MONGODBURI is defined
-if (!mongodbURI) {
-    console.error('MONGODBURI environment variable is not defined');
-    process.exit(1); // Exit process with failure code
-}
-
-// Middleware setup
+//middlewares
 app.use(cors());
 app.use(express.json());
-app.use('/bookings', bookingRouter);
-app.get('/jsonfile', (req, res) => {
-    const filePath = path.join(__dirname, 'data.json'); // Adjust path as necessary
+app.use("/bookings", bookingRouter);
 
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error('Error reading file:', err);
-            res.status(500).json({ message: 'Error reading file' });
-            return;
-        }
+mongoose.connect(mongodbURI)
+    .then(() =>
+        app.listen(PORT, () =>
+            console.log(`Connected to mongodb database and server is running on port: ${PORT}`)
+        )
 
-        res.setHeader('Content-Type', 'application/json');
-        res.send(data);
-    });
-});
-// Connect to MongoDB and start the server
-mongoose.connect(mongodbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        app.listen(PORT, () => {
-            console.log(`Connected to MongoDB database and server is running on port: ${PORT}`);
-        });
-    })
-    .catch((err) => {
-        console.error('Failed to connect to MongoDB:', err.message);
-        process.exit(1); // Exit process with failure code
-    });
-
-
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-    console.error('Server error:', err.stack);
-    res.status(500).json({ message: 'Internal Server Error' });
-});
+    ).catch((err) => console.log(err))
