842fb38ddf70451d6dc762f22f3fda4f427e9f15
Garima Majhi <garima691wiz@gmail.com>
2024-08-12 01:48:57 +0530

    movie

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,22 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import bookingRouter from './routes/bookingRoutes.js';
-
-// Load environment variables
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import bookingRouter from "./routes/bookingRoutes.js";
 dotenv.config();
 
+
+//paste mongodbURI = "mongodb://127.0.0.1:27017/bookings" 
+
+const mongodbURI = process.env.MONGODBURI
 const app = express();
-const PORT = process.env.PORT || 8080;
-const mongodbURI = process.env.MONGODBURI;
+const PORT = 8080;
 
-// Check if MONGODBURI is defined
-if (!mongodbURI) {
-    console.error('MONGODBURI environment variable is not defined');
-    process.exit(1); // Exit process with failure code
-}
-
-// Middleware setup
+//middlewares
 app.use(cors());
 app.use(express.json());
-app.use('/bookings', bookingRouter);
+app.use("/bookings", bookingRouter);
 
-// Connect to MongoDB and start the server
 mongoose.connect(mongodbURI)
     .then(() =>
         app.listen(PORT, () =>
@@ -30,4 +24,3 @@ mongoose.connect(mongodbURI)
         )
 
     ).catch((err) => console.log(err))
-
